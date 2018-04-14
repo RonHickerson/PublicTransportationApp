e80b1cdf37e529e26646ae68eeaf3a427779062b
Ron <RonaldJHickerson@yahoo.com>
2018-04-14 16:51:14 -0500

    Create Linked List

---
+++ src/com/company/LList.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class LList {
+    public int value;
+    public LList next;
+
+    public LList(int value, LList next) {
+        this.value = value;
+        this.next = next;
+    }
+}
